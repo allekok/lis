237884d394a85fe2449e99ae313adc9387ac0735
Payam <payambapiri.97@gmail.com>
2021-05-03 07:36:37 +0430

    lis.php: Simplifying.

--- lis.php
+++ lis.php
@@ -9,28 +9,28 @@
 repl("> ");
 
 /* Functions */
-function read_from_tokens (&$tokens) {
-	if(!$tokens) return;
+function read_from_tokens(&$tokens) {
 	$token = array_shift($tokens);
 	if($token == "(") {
 		$L = [];
-		while($tokens && $tokens[0] != ")")
+		while($tokens[0] != ")")
 			$L[] = read_from_tokens($tokens);
 		array_shift($tokens);
 		return $L;
 	}
-	elseif($token == ")") return;
-	else return $token;
+	else
+		return $token;
 }
-function get_env ($k, $env) {
-	if(isset($env[$k])) return $env[$k];
-	elseif(isset($env["parent"])) return get_env($k, $env["parent"]);
-	return $k;
+function get_env($key, $env) {
+	if(isset($env[$key]))
+		return $env[$key];
+	elseif(isset($env["parent"]))
+		return get_env($key, $env["parent"]);
+	return $key;
 }
-function _read ($prompt) {
+function _read($prompt) {
 	readline_add_history($str = readline($prompt));
-	$tokens = preg_split("/\s+/u",
-			     str_replace(["(",")"], [" ( ", " ) "], $str),
+	$tokens = preg_split("/\s+/", preg_replace("/([\(\)])/", " $1 ", $str),
 			    -1, PREG_SPLIT_NO_EMPTY);
 	return read_from_tokens($tokens);
 }
@@ -41,19 +41,20 @@ function _eval ($exp, &$env) {
 		return $env[$exp[1]] = _eval($exp[2], $env);
 	elseif("lambda" == $exp[0])
 		return ["closure", ["parent" => &$env], $exp[1], $exp[2]];
-	else { /* Apply */
+	else {
 		foreach($exp as $i => $o)
 			$exp[$i] = _eval($o, $env);
-		$proc = $exp[0];
-		if($proc[0] != "closure") return $exp;
+		$proc = array_shift($exp);
 		foreach($proc[2] as $i => $f)
-			$proc[1][$f] = $exp[$i+1];
+			$proc[1][$f] = $exp[$i];
 		return _eval($proc[3], $proc[1]);
 	}
 }
 function _print ($x) {
-	if(is_array($x)) print_r($x);
-	else echo "$x\n";
+	if(is_array($x))
+		print_r($x);
+	else
+		echo "$x\n";
 }
 function repl ($prompt, $env=[]) {
 	for(;;)	_print(_eval(_read($prompt), $env));
