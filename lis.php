a741dedfae4dcaf41e3c2e223b29b8846da67294
Payam <payambapiri.97@gmail.com>
2021-05-23 03:56:25 +0430

    lis.php: Remove un-necessary 'elses'.

--- lis.php
+++ lis.php
@@ -16,22 +16,19 @@ function read_from_tokens(&$tokens) {
 		array_shift($tokens);
 		return $L;
 	}
-	else
-		return $token;
+	return $token;
 }
 function _eval($exp, &$env) {
 	if(!is_array($exp))
 		return get($exp, $env);
 	elseif("label" == $exp[0])
-		$env[$exp[1]] = _eval($exp[2], $env);
+		return $env[$exp[1]] = _eval($exp[2], $env);
 	elseif("lambda" == $exp[0])
 		return [$exp[1], $exp[2], ["dad" => &$env]];
-	else {
-		$exp[0] = _eval($exp[0], $env);
-		foreach($exp[0][0] as $i => $f)
-		$exp[0][2][$f] = _eval($exp[$i + 1], $env);
-		return _eval($exp[0][1], $exp[0][2]);
-	}
+	$exp[0] = _eval($exp[0], $env);
+	foreach($exp[0][0] as $i => $f)
+	$exp[0][2][$f] = _eval($exp[$i + 1], $env);
+	return _eval($exp[0][1], $exp[0][2]);
 }
 function get($key, $env) {
 	return isset($env[$key]) ? $env[$key] :
