d47cbfc81be2d686c7b84a5199a2362ddb3513a7
Payam <payambapiri.97@gmail.com>
2021-06-09 23:06:17 +0430

    lis.php: Fix a bug. Introducing Execution environments.

--- lis.php
+++ lis.php
@@ -8,11 +8,12 @@ function T(&$T) {
 function R($P) { return @T(preg_split("/(\(|\))| /", readline($P), -1, 3)); }
 function E($X, &$E) {
 	if(!is_array($X))
-		return @$E[$X] ? $E[$X] : (@$E[".."] ? E($X, $E[".."]) : $X);
+		return @$E[$X] ? $E[$X] : (@$E["."] ? E($X, $E["."]) : $X);
 	elseif("label" == $X[0]) return $E[$X[1]] = E($X[2], $E);
-	elseif("lambda" == $X[0]) return [$X[1], $X[2], [".." => &$E]];
+	elseif("lambda" == $X[0]) return [$X[1], $X[2], ["." => &$E]];
 	$X[0] = E($X[0], $E);
-	foreach($X[0][0] as $i => $f) $X[0][2][$f] = E($X[$i + 1], $E);
-	return E($X[0][1], $X[0][2]);
+	$F["."] = $X[0][2];
+	foreach($X[0][0] as $i => $f) $F[$f] = E($X[$i + 1], $E);
+	return E($X[0][1], $F);
 }
 function L($P, $E=[]) { for(;;) echo print_r(E(R($P),$E), 1), "\n"; }
