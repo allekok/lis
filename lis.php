f269c12594f2beedb0687f17c8632cd688650cbf
Payam <payambapiri.97@gmail.com>
2021-06-13 03:43:33 +0430

    lis.php: Re-position "R" func.

--- lis.php
+++ lis.php
@@ -1,11 +1,11 @@
 <?php L("> ");
+function R($P) { return @T(preg_split("/(\(|\))| /", readline($P), -1, 3)); }
 function T(&$T) {
 	if(($t = array_shift($T)) != "(") return $t;
 	while($T[0] != ")") $L[] = T($T);
 	array_shift($T);
 	return $L;
 }
-function R($P) { return @T(preg_split("/(\(|\))| /", readline($P), -1, 3)); }
 function E($X, &$E) {
 	if(!is_array($X))
 		return @$E[$X] ? $E[$X] : (@$E["."] ? E($X, $E["."]) : $X);
